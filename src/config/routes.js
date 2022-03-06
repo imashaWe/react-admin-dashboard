106e330929bccc94922e0545fc52adecde14311a
imasshaWe <imasha98.we@gmail.com>
2022-03-06 11:56:53 +0530

    add 404 page

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,8 +1,13 @@
 import Home from "../components/pages/Home";
 import SystemUsers from "../components/pages/SystemUsers";
 import BlankPage from "../components/pages/BlankPage";
+import Page404 from "../components/pages/Page404";
 
 export const routes = [
+    {
+        path: "*",
+        element: <Page404/>
+    },
     {
         path: "/",
         element: <Home/>
